f414a68563a5619f96fcd2338c98cbdc8c183e5c
Drew Ayling <drew.ayling@livongo.com>
2021-04-14 14:50:24 -0600

    use submodule instead for easy releasability

--- python/setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools.config import read_configuration
-
-conf_dict = read_configuration('/home/user/dev/package/setup.cfg')
--- python/src/testrail/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-if sys.version_info >= (3, 0):
-    from testrail._py3 import *
-elif sys.version_info < (3,0):
-    from testrail._py2 import *
